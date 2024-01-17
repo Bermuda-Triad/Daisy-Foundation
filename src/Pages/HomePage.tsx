cf82f9180d8b42185ebbc5bce0c4285fbcf2f872
Parlgy <parlg4776@gmail.com>
2024-01-17 14:45:38 +0300

    projects section

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -2,6 +2,10 @@ import Hero from "../assets/images/Hero.png";
 import Logo1 from "../assets/images/Logos/flat-logo.svg";
 import Mission from "../assets/images/illustrations/mission.svg";
 import Vision from "../assets/images/illustrations/vision.svg";
+import Objectives from "../assets/images/objective.png";
+import Check from "../assets/images/check.svg";
+import ProjectImageOne from "../assets/images/Projects/front-image-1.png";
+import ProjectImageTwo from "../assets/images/Projects/front-image-2.png";
 
 const HomePage = () => {
   return (
@@ -97,6 +101,74 @@ const HomePage = () => {
                 <p className="font-bold text-2xl text-center">Husika</p>
               </div>
             </div>
+
+            <div className="py-20">
+              <div className="lg:flex gap-16 items-center">
+                <img
+                  src={Objectives}
+                  alt=""
+                  className="border border-strokeOne"
+                />
+                <div>
+                  <div className="mb-4 mt-4 lg:mt-0">
+                    <p className="uppercase font-semibold mb-1 text-textTwo">
+                      Daisy Nyongesa Foundation
+                    </p>
+                    <div className="w-20 h-2 bg-accentOne rounded-full "></div>
+                  </div>
+                  <h1 className="lg:col-span-2 text-textOne font-bold lg:font-black text-4xl lg:text-6xl leading-none text-left">
+                    Our Objectives
+                  </h1>
+                  <div className="mt-8 lg:w-3/4">
+                    <p className="px-6 mb-4 py-2 font-semibold text-textOne text-xl bg-accentBackgroundOne border-l-accentOne border-l-4 rounded-lg mt-4">
+                      Our Objectives Include:
+                    </p>
+                    <OurObjectives
+                      description={
+                        "To champion for social inclusion of women and youth."
+                      }
+                    />
+                    <OurObjectives
+                      description={
+                        "To implement programs that fulﬁl people's basic needs."
+                      }
+                    />
+                    <OurObjectives
+                      description={
+                        "To create a sense of collaboration between government and society."
+                      }
+                    />
+                    <OurObjectives
+                      description={
+                        "To advocate for economic growth and sustainability through climate change"
+                      }
+                    />
+                    <OurObjectives
+                      description={"To actualize mentorship academy."}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-accentBackgroundOne">
+            <div className="px-4 py-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-2xl">
+              <div className="mb-4 mt-4 lg:mt-0">
+                <p className="uppercase font-semibold mb-1 text-textTwo">
+                  Daisy Nyongesa Foundation
+                </p>
+                <div className="w-20 h-2 bg-accentOne rounded-full "></div>
+              </div>
+              <h1 className="lg:col-span-2 text-textOne font-bold lg:font-black text-4xl lg:text-6xl leading-none text-left">
+                Our Projects
+              </h1>
+              <div className="mt-16">
+                <ProjectOne />
+                <div className="py-10"></div>
+                <ProjectTwo />
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -105,3 +177,62 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+interface ObjectivesProps {
+  description: string;
+}
+
+export const OurObjectives = (props: ObjectivesProps) => {
+  return (
+    <div className="flex items-start mt-2 gap-2 text-lg text-textTwo">
+      <img src={Check} alt="" />
+      <p>{props.description}</p>
+    </div>
+  );
+};
+
+export const ProjectOne = () => {
+  return (
+    <div>
+      <div className="lg:flex gap-20 items-center">
+        <img src={ProjectImageOne} alt="" />
+        <div className="">
+          <p className="mb-4">Donations / Farming</p>
+          <p className="lg:text-4xl text-2xl font-bold">
+            DAISY IN THE FARM + MFCS Visit.
+          </p>
+          <p className="mt-2 lg:mt-6 text-lg">
+            Daisy Nyongesa Foundation is dedicated to empowering youth and women
+            through Education, Mentorship, Entrepreneurship, and Leadership,
+            fostering a future where every voice leads and inspires.
+          </p>
+          <button className="text-accentTwo py-4 mt-8 border-2 border-accentTwo px-12 text-lg font-semibold rounded-lg">
+            View Gallery
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const ProjectTwo = () => {
+  return (
+    <div>
+      <div className="flex flex-col-reverse lg:flex-row lg:gap-20 items-center">
+        <div className="">
+          <p className="mb-4">Donations / Persons with Disability</p>
+          <p className="lg:text-4xl text-2xl font-bold">Wheelchair Donation.</p>
+          <p className="lg:mt-6 mt-2 text-lg">
+            Daisy Nyongesa Foundation is dedicated to empowering youth and women
+            through Education, Mentorship, Entrepreneurship, and Leadership,
+            fostering a future where every voice leads and inspires.
+          </p>
+          <button className="text-accentTwo py-4 mt-8 border-2 border-accentTwo px-12 text-lg font-semibold rounded-lg">
+            View Gallery
+          </button>
+        </div>
+        <img src={ProjectImageTwo} alt="" />
+      </div>
+    </div>
+  );
+};
